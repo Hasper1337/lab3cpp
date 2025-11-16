1bf785188585b77dc2386f4f8f5b2f8d0365f2ce
Viner <Viner@HASPER>
2025-11-16 22:25:46 +0500

    Добавьте файлы проекта.

---
+++ lab3/lab3.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class MusicalInstrument {
+public:
+
+	void virtual play() {
+		cout << "Musical instrument sound like ";
+	};
+
+	void tune() {
+
+	};
+
+	void getType() {
+
+	};
+
+};
+
+class StringInstrument : MusicalInstrument {
+	void strings() { // струны
+
+	};
+
+	void tension() { // натяжение
+
+	};
+};
+
+class WindInstrument : MusicalInstrument {
+	void diaphragm() { // диафрагма
+
+	};
+	
+	void material() { // материал
+
+	};
+};
+
+class Guitar {
+
+};
+
+class Violin {
+
+};
+
+class Flute {
+
+};
+
+class SynthGuitar : Guitar { //ещё IElectronicDevice??
+
+};
+
+int main() {
+
+}
